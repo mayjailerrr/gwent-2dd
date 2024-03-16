1ea71e58e5fa1c7ae27ef7c82025c8e20777e2cc
mayjailerr <159580416+mayjailerr@users.noreply.github.com>
2024-03-16 14:29:01 -0400

    esto fue tremendo, tuve que borrar el proyecto y empezar desde cero, so hay un monton de cambios y principalmente que decidi terminar la PlayerHand sin la otra para en un punto duplicate y ahorrar time

--- Assets/Draw.cs
+++ Assets/Draw.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 
 public class Draw : MonoBehaviour
 {
+    public GameObject PlayerArea;
     public GameObject Card1;
-    public GameObject Card2;
-    public GameObject MyHand;
-    public GameObject EnemyHand;
+
+    List <GameObject> cards = new List<GameObject>();
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       cards.Add(Card1); 
     }
+
     public void OnClick()
     {
         for(var i = 0; i < 10; i++)
         {
-            GameObject playerCard = Instantiate(Card1, new Vector2(0, 0), Quaternion.identity);
-            playerCard.transform.SetParent(MyHand.transform, false);
+            GameObject playerCard = Instantiate(cards[Random.Range(0, cards.Count)], new Vector2(0,0), Quaternion.identity);
+            playerCard.transform.SetParent(PlayerArea.transform, false);
         }
     }
+
 }
