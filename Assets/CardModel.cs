53a34825bf49f2151cff268cb8805374eea8f0bd
mayjailerr <159580416+mayjailerr@users.noreply.github.com>
2024-04-01 06:58:36 -0400

    bueno, bueno, en la boberia hicimos un main menu basicamente porque si y, importante, la clase carta para colocar los datos, faltarian de forma importante los turnos, las rondas, configurar lo del cementerio y los metoditos de suma, resta y tal, fuerza rusia eu confio

---
+++ Assets/CardModel.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardModel : MonoBehaviour
+{
+    public string Name;
+    public string TypeOfCard;
+    public int Power;
+    public string TypeOfPower;
+    public int Faction;
+    public string Index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
